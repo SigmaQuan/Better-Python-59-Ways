a9d96b73066c59987d95377d0fc7493e7595c798
robot steven <quan.zhibin@gmail.com>
2017-02-07 21:18:20 +0800

    item 01 : Know which version of Python you're using.

---
+++ item_02_PEP8Style.py
@@ -0,0 +1,51 @@
+#
+# Whitespace: In Python, whitespace is syntactically significant. Python
+# programmers are especially sensitive to the effects of whitespace on
+# code clarity.
+
+# Use spaces instead of tabs for indentation.
+
+# Use four spaces for each level of syntactically significant indenting.
+
+# Lines should be 79 characters in length or less.
+
+# Continuations of long expressions onto additional lines should be indented
+# by four extra spaces from their normal indentation level.
+
+# In a file, functions and classes should be separated by two blank lines.
+
+# In a class, methods should be separated by one blank line.
+
+# Don't put spaces around list indexes, function calls, or keyword argument
+# assignments.
+
+# Put one-and only one-space before and after variable assignments.
+
+
+# Naming: PEP 8 suggests unique styles of naming for different part in the
+# language.
+
+# Functions, variables, and attributs should be in lovercase_underscore format.
+
+# Protected instance attributes should be in _leading_underscore format.
+
+# Private instance attributes should be in __double_leading_underscore format.
+
+# Classes and exceptions should be in CapitalizedWord format.
+
+# Module-level constants should be in ALL_CAPS format.
+
+# Instance methods in classes should use self as the name of the first
+# parameter (which refers to the object).
+
+# Class methods should use cls as the name of the first parameter (which
+# refers to the class).
+
+
+# Expersssions and Statements:
+
+
+
+# Things to Remember
+
+
