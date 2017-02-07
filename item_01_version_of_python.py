a34eef0f0596767bb0161faff7a021607269a189
robot steven <quan.zhibin@gmail.com>
2017-02-07 20:49:26 +0800

    item 01 : Know which version of Python you're using.

---
+++ item_01_version_of_python.py
@@ -0,0 +1,19 @@
+# $ python --version
+# Python 2.7.12
+#
+# $ python3 --version
+# Python 3.5.2
+
+
+import sys
+print(sys.version_info)
+# sys.version_info(major=2, minor=7, micro=12, releaselevel='final', serial=0)
+
+print(sys.version)
+# 2.7.12 (default, Nov 19 2016, 06:48:10)
+# [GCC 5.4.0 20160609]
+
+
+# # Things to Remember
+# There are two major version of Python still in active use: Python 2 and Python 3
+# There are
