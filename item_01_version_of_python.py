afcef3297640d1097b19a8b1f6f1a3064c1eaa85
robot steven <quan.zhibin@gmail.com>
2017-02-07 20:52:41 +0800

    item 01 : Know which version of Python you're using.

--- item_01_version_of_python.py
+++ item_01_version_of_python.py
@@ -15,5 +15,7 @@ print(sys.version)
 
 
 # # Things to Remember
-# There are two major version of Python still in active use: Python 2 and Python 3
-# There are
+# There are two major version of Python still in active use: Python 2 and Python 3.
+# There are multiple popular runtimes for Python: CPython, Jython, IronPython, PyPy, etc.
+# Be sure that the command-line for running Python on your system is the version you expect it to be.
+# Prefer Python 3 for your next project because that is the primary focus of the Python community.
