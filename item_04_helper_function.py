4cca9019c1bd290b475bd97b4f2143eb3c7a9acc
robot steven <quan.zhibin@gmail.com>
2017-02-09 11:51:55 +0800

    item 01 : Know which version of Python you're using.

--- item_04_helper_function.py
+++ item_04_helper_function.py
@@ -7,7 +7,6 @@
 # value:
 
 
-# Python 3
 from urllib.parse import parse_qs
 my_values = parse_qs('red=5&blue=0&green=', keep_blank_values=True)
 print(repr(my_values))
@@ -34,3 +33,39 @@ print("Opacity: ", my_values.get('opacity'))
 # supplied or is blank. You might choose to do this with Boolean expressions
 # because it feels like this logic doesn't merit a whole if statement or
 # helper function quite yet.
+
+# Python's syntax makes this choice all too easy. The trick here is that the
+# empty string, the empty list, and zero all evaluate to False implicitly.
+# Thus, the expressions below will evaluate to the subexpression after the or
+# operator when the first subexpression is False.
+
+
+# For query string 'red=5&blue=0&green='
+red = my_values.get('red', [''])[0] or 0
+green = my_values.get('green', [''])[0] or 0
+opacity = my_values.get('opacity', [''])[0] or 0
+print("Red:     %r" % red)
+print("Green:   %r" % green)
+print("Opacity: %r" % opacity)
+# Red:     '5'
+# Green:   0
+# Opacity: 0
+
+
+# The red case works because the key is present in the my_values dictionary.
+# The value is a list with one member: the string '5'. This string implicitly
+# evaluates to True, so red is assigned to the first part of the or
+# expression.
+
+# The green case works because the value in the my_values dictionary is a list
+# with one member: an empty string. The empty string implicitly evaluates to
+# False, causing the or expression to evaluate to 0.
+
+# The opacity case works because the value in the my_values dictionary is
+# missing altogether. The behavior of the get method is to return its second
+# argument if the key doesn't exist in the dictionary. The default value in
+# this case is a list with one member, an empty string. When opacity isn't
+# found in the dictionary, this code does exactly the same thing as the green
+# case.
+
+# 
