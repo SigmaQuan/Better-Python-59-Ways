e21861dc5c1758597f4e0cbe5d05c20593e7cf48
robot steven <quan.zhibin@gmail.com>
2017-02-14 19:17:27 +0800

    Chapter 1: Pythonic thinking    Item 1: Know which version of python you're using

--- item_01_version_of_python.py
+++ item_01_version_of_python.py
@@ -1,6 +1,21 @@
 # Chapter 1: Pythonic thinking
 
 
+# The idioms of a programming language are defined by its users. Over the
+# years, the Python community has come to use the adjective Pythonic to
+# describe code that follows a particular style. The Pythonic style isn't
+# regimented or enforced by the compiler. It has emerged over time through
+# experience using the language and working with others. Python programmers
+# prefer to be explicit, to choose simple over complex, and to maximize
+# readability (type import this).
+
+# Programmers familiar with other language may try to write Python as if it's
+# C++, Java, or whatever know best. New programmers may still be getting
+# comfortable with the vast range of concepts expressible in Python. It's
+# important for everyone to know the best--the Pythonic--way to do the most
+# common things in Python. These patterns will affect every program you write.
+
+
 # Item 1: Know which version of python you're using
 
 
