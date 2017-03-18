4b62d29a126ad47b9e6aa8a0151c2c32b864f579
robot steven <quan.zhibin@gmail.com>
2017-03-18 14:53:40 +0800

    Item 28: Inherit from collections.abc for custom container types

--- item_28_inherit_from_collections_abc.py
+++ item_28_inherit_from_collections_abc.py
@@ -45,7 +45,7 @@ print('Frequency:', foo.frequency())
 # sequence semantic (like list or tuple) for a binary tree class.
 
 
-class BianryNode(object):
+class BinaryNode(object):
     def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
@@ -71,6 +71,144 @@ print(bar.__getitem__(0))
 # To make the BinaryNode class act like a sequence, you can provide a custom
 # implementation of __getitem__ that traverses the object tree depth first.
 
+
+class IndexableNode(BinaryNode):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def _search(self, count, index):
+        found = False
+        return (found, count)
+        # ...
+        # returns (found, count)
+
+    def __getitem__(self, index):
+        found, _ = self._search(0, index)
+        if not found:
+            raise IndexError("Index out of range")
+        return found.value
+
+
+# You can construct your binary tree as usual.
+
+
+tree = IndexableNode(
+    10,
+    left=IndexableNode(
+        5,
+        left=IndexableNode(2),
+        right=IndexableNode(
+            6,
+            right=IndexableNode(7)
+        )
+    ),
+    right=IndexableNode(
+        15, left=IndexableNode(11)
+    )
+)
+
+
+# But you can also access it like a list in addition to tree traversal.
+
+
+print('LRR', tree.left.right.right.value)
+# print('Index 0 = ', tree[0])
+# print('Index 1 = ', tree[1])
+# print('11 in the tree?', 11 in tree)
+# print('17 in the tree?', 17 in tree)
+# print('Tree is ', list(tree))
+
+
+# The problem is that implementing __getitem__ isn't enough to provide all of
+# the sequence semantics you'd expect.
+
+
+# len(tree)
+# TypeError: object of type 'IndexableNode' has no len()
+
+
+# The len built-in function requires another special method named __len__ that
+# must have an implementation for your custom sequence type.
+
+
+class SequenceNode(IndexableNode):
+    def __len__(self):
+        _, count = self._search(0, None)
+        return count
+
+tree = IndexableNode(
+    10,
+    left=IndexableNode(
+        5,
+        left=IndexableNode(2),
+        right=IndexableNode(
+            6,
+            right=IndexableNode(7)
+        )
+    ),
+    right=IndexableNode(
+        15, left=IndexableNode(11)
+    )
+)
+
+print('Tree has %d nodes' % len(tree))
+
+
+# Unfortunately, this still isn't enought. Also missing are the count and
+# index methods that a Python programmer would expect to see on a sequence
+# like list or tuple. Defining your own container types is much harder than
+# it looks.
+
+# To avoid this difficulty throughout the Python universe, the built-in
+# collections.abc mudule defines a set of abstract base classes that provide
+# all of the typical methods for each container type. When you subclass from
+# these abstract base classes and forget to implement required methods, the
+# module will tell you something is wrong.
+
+
+# from collections.abc import Sequence
+from collections import Sequence
+
+
+class BadType(Sequence):
+    pass
+
+foo = BadType()
+# TypeError: Can't instantiate abstract class BadType with abstract methods __getitem__, __len__
+
+
+# When you do implement all of the methods required by an abstract base class,
+# as I did above with SequenceNode, it will provide all of the additional
+# methods like index and count for free.
+
+
+class BetterNode(SequenceNode, Sequence):
+    pass
+
+tree = IndexableNode(
+    10,
+    left=IndexableNode(
+        5,
+        left=IndexableNode(2),
+        right=IndexableNode(
+            6,
+            right=IndexableNode(7)
+        )
+    ),
+    right=IndexableNode(
+        15, left=IndexableNode(11)
+    )
+)
+
+print('Index of 7 is', tree.index(7))
+print('Count of 10 is', tree.count(10))
+
+
+# The benefit of using these abstract base class is even greater for more
+# complex types like Set and MutableMapping, which have a large number of
+# special methods that need to be implemented to match Python conventions.
 
 
 # Things to remember
