622758c9d664a0ff55295c825b19f43c3a84bc25
robot steven <quan.zhibin@gmail.com>
2017-05-13 21:02:17 +0800

    Item 39: Use queue to coordinate work between threads

--- item_39_use_queue.py
+++ item_39_use_queue.py
@@ -1,4 +1,8 @@
 # Item 39: Use queue to coordinate work between threads
+from collections import deque
+from threading import Lock
+from threading import Thread
+from time import sleep
 
 
 # Python programs that do many things concurrently often need to coordinate
@@ -29,6 +33,97 @@
 # Item 38: "Use lock to prevent data races in threads" to understand the
 # importance of thread safely in Python; see Item 46: "Use built-in algorithms
 # and data structures" for the deque class).
+
+class MyQueue(object):
+    def __int__(self):
+        self.items = deque()
+        self.lock = Lock()
+
+# The producer, your digital camera, adds new images to the end of th list of
+# pending items.
+    def put(self, item):
+        with self.lock:
+            self.items.append(item)
+
+# The consumer, the first phase of your processing pipeline, removes images
+# from the front of the list of pending items.
+    def get(self):
+        with self.lock:
+            return self.items.popleft()
+
+
+# Here, I represent each phase of the pipeline as a Python thread that takes
+# work from one queue like this, runs a function on it, and puts the results
+# on another queue. I also track how many times the worker has checked for new
+# input and how much work it's completed.
+
+
+class Worker(Thread):
+    def __init__(self, func, in_queue, out_queue):
+        super().__init__()
+        self.func = func
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+        self.polled_count = 0
+        self.work_done = 0
+
+# The trickiest pat is that the worker thread must properly handle the case
+# where the input queue is empty because the previous phase hasn't completed
+# its work yet. This happens where I catch the Index Error exceptions below.
+# You can think of this as a holdup in the assembly line.
+    def run(self):
+        while True:
+            self.polled_count += 1
+            try:
+                item = self.in_queue.get()
+            except IndexError:
+                sleep(0.01)  # No work to do
+            else:
+                result = self.func(item)
+                self.out_queue.put(result)
+                self.work_done += 1
+
+
+# Now I can connect the three phases together by creating the sequence for
+# their coordination point and the corresponding worker threads.
+
+download_queue = MyQueue()
+resize_queue = MyQueue()
+upload_queue = MyQueue()
+done_queue = MyQueue()
+threads = [
+    Worker(download, download_queue, resize_queue),
+    Worker(resize, resize_queue, upload_queue),
+    Worker(upload, upload_queue, done_queue),
+]
+
+
+# I can start the threads and then inject a bunch of work into the first
+# phase of the pipeline. Here, I use a plain object instance as a porxy for
+# the real data required by the download function:
+
+
+for thread in threads:
+    thread.start()
+for _ in range(1000):
+    download_queue.put(object())
+
+# Now I wait for all of the items to be processed by the pipeline and end up
+# in the done_queue.
+
+
+while len(done_queue.items) < 1000:
+    # Do something useful while waiting
+    # ....
+
+# This runs properly, but there's an interesting side effect caused by the
+# threads polling their input queues for new work. The tricky part, where I
+# catch IndexError exceptions in the run method, executes a large number of
+# items.
+
+processed = len(done_queue.items)
+polled = sum(t.polled_count for t in threads)
+print('Processed', processed, 'items after polling', polled, 'items')
 
 
 
