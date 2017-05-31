dae3fed5bfef0d534f49417a4c96888ba64b7bac
robot steven <quan.zhibin@gmail.com>
2017-05-31 21:07:42 +0800

    Item 41: Consider concurrent.futures for true parallelism

--- item_41_consider_concurrent_futures.py
+++ item_41_consider_concurrent_futures.py
@@ -1,6 +1,7 @@
 # Item 41: Consider concurrent.futures for true parallelism
 import time
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 
 # At some point in writing Python programs, you may hit the performance wall.
@@ -88,9 +89,96 @@ print('Took %.3f seconds' % (end - start))
 # Took 0.680 seconds
 
 
+# Running this code on multiple Python threads will yield no speed improvement
+# because the GIL prevents Python from using multiple CPU cores in parallel.
+# Here, I do the same computation as above using the concurrent.futures module
+# with its ThreadPoolExecutor class and two worker threads (to match the
+# number of CPU cores on my computer):
+
+start = time.time()
+pool = ThreadPoolExecutor(max_workers=2)
+results = list(pool.map(gcd, numbers))
+end = time.time()
+print('Took %.3f seconds' % (end - start))
+# Took 0.664 seconds
+
+
+# It's even slower this time because of the overhead of starting and
+# communicating with the pool of threads.
+
+# Now for the surprising part: By changing a single line of code, something
+# magical happens. If I replace the ThreadPoolExecutor with the
+# ProcessPoolExecutor from the concurrent.futures module, everything speeds
+# up.
+
+start = time.time()
+pool = ProcessPoolExecutor(max_workers=2)  # the one change
+results = list(pool.map(gcd, numbers))
+end = time.time()
+print('Took %.3f seconds' % (end - start))
+# Took 0.357 seconds
+
+# Running on my dual-core machine, it's significantly faster! How is this
+# possible? Here's what the ProcessPoolExecutor class actually does (via the
+# low-level constructs provided by the multiprocessing module):
+#    1. It takes each item from the numbers input data to map.
+#    2. It serializes it into binary data using the pickle module (see Item 44:
+#       "Make pickle reliable with copyreg").
+#    3. It copies the serialized data from the main interpreter process to a
+#       child interpreter process over a local socket.
+#    4. Next, it de-serializes the data back into Python objects using pickle
+#       in the child process.
+#    5. It then imports the Python module containing the gcd function.
+#    6. It runs the function on the input data in parallel with other child
+#       processes.
+#    7. It serializes the result back into bytes.
+#    8. It copies those bytes back through the socket.
+#    9. It de-serializes the bytes back into Python objects in the parent
+#       process.
+#   10. Finally, it merges the results from multiple children into a single
+#       list to return.
+
+# Although it looks simple to the programmer, the multiprocessing module and
+# ProcessPoolExecutor class do a huge amount of work to make parallelism
+# possible. In most other language, the only touch point you need to
+# coordinate two threads is a single lock or atomic operation. The overhead of
+# using multiprocessing is high because of all of the serialization and
+# deserialization that must happen between the parent and child processes.
+
+# This scheme is well suited to certain types of isolated, high-leverage
+# tasks. By isolated, I mean function that don't need to share state with
+# other parts of the program. By high-leverage, I mean situations in which
+# only a small amount of data must be transferred between the parent and
+# child processes to enable a large amount of computation. The greatest
+# common denominator algorithm is one example of this, but many other
+# mathematical algorithms work similarly.
+
+# If your computation doesn't have these characteristics, then the overhead of
+# multiprocessing may prevent it from speeding up your program through
+# parallelization. When that happens, multiprocessing provides more advanced
+# facilities for shared memory, cross-process locks, queues, and proxies. But
+# all of these features are very complex. It's hard enough to reason about
+# such tools in the memory space of a single process shared between Python
+# threads. Extending that complexity to other processes and involving sockets
+# makes this much more difficult to understand.
+
+# I suggest avoiding all part of multiprocessing and using these features via
+# the simpler concurrent.futures modules module. You can start by using the
+# ThreadPoolExecutor class to run isolated, high-leverage functions in
+# threads. Later, you can move to the ProcessPoolExecutor to get a speedup.
+# Finally, once you've completely exhausted the other options, you can
+# consider using the multiprocessing module directly.
+
+
 # Things to remember
 
-# 1.
-# 2.
-# 3.
-# 4.
+# 1. Moving CPU bottlenecks to C-extension modules can be an effective way to
+#    improve performance while maximizing your investment in Python code.
+#    However, the cost of doing so is high and may introduce bugs.
+# 2. The multiprocessing module provides powerful tools that can parallelize
+#    certain types of Python computation with minimal effort.
+# 3. The power of multiprocessing is best accessed through the
+#    concurrent.futures built-in module and its simple ProcessPoolExecutor
+#    class.
+# 4. The advanced parts of the multiprocessing module should be avoided
+#    because they are so complex.
