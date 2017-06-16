0c4831fa02c2de39aa320e4177cee553ad4ce608
robot steven <quan.zhibin@gmail.com>
2017-06-16 12:50:17 +0800

    Chapter 8: Production Item 54: Consider module-scoped code to configure deployment environments

---
+++ item_54_consider_module_scoped_code.py
@@ -0,0 +1,18 @@
+# Chapter 8: Production
+
+
+# Putting a Python program to use requires moving it from a development
+# environment to a production environment. Supporting disparate configurations
+# like this can be a challenge. Making programs that are dependable in
+# multiple situations is just as important as making programs with correct
+# functionality.
+
+# The goal is to productionize your Python programs and make them bulletproof
+# while they're in use. Python has built-in modules that aid in hardening your
+# programs. It provides facilities for debugging, optimizing, and testing to
+# maximize the quality and performance of your programs at runtime.
+
+
+# Item 54: Consider module-scoped code to configure deployment environments
+
+#
